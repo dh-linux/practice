0b981af7598d587d7151106a88b3d3482fb24122
dh-linux <dh_linux@163.com>
2016-05-06 01:15:42 +0800

    updated

--- python/python/practice/kill.py
+++
@@ -1,8 +0,0 @@
-#coding:utf-8
-import os  
-import signal  
-   
-#发送信号，16175是前面那个绑定信号处理函数的pid，需要自行修改  
-os.kill(25577,signal.SIGTERM)  
-#发送信号，16175是前面那个绑定信号处理函数的pid，需要自行修改  
-os.kill(25577,signal.SIGUSR1) 
